50e18392f4c8f4e5fc6b8c00494ff1f87c4760ee
Akshaya T P <86123414+akshutp@users.noreply.github.com>
2024-10-30 15:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "{\"message\":\"Hey there,this is Akshaya\"}"
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=int("5000"),debug=True)
+    
 
